8947aa32160e87d2cecc48fefca8753842ab44a7
Gaurav Munjal <gmunjal@uplandsoftware.com>
2021-11-16 09:12:15 -0500

    avoid deprecation warning

--- addon/utils/style-computed.js
+++ addon/utils/style-computed.js
@@ -3,7 +3,14 @@ import { get, computed } from '@ember/object';
 
 export default prop =>
   computed(`${prop}`, function() {
+    const obj = get(this, prop);
     return htmlSafe(
-      Object.keys(get(this, prop)).reduce((acc, key) => `${acc} ${key}: ${get(this, `${prop}.${key}`)};`, '')
+      Object.keys(obj).reduce((acc, key) => {
+        if (key !== '@each') {
+          // avoid deprecation
+          return `${acc} ${key}: ${get(this, `${prop}.${key}`)};`;
+        }
+        return acc;
+      }, '')
     );
   });
