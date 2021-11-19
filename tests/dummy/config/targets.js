56b7cf23907d201e3682bc7ebcdadf9cdefaa0cf
Gaurav Munjal <gmunjal@uplandsoftware.com>
2021-11-19 15:16:46 -0500

    ember-cli-update to 3.24

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+const browsers = [
+  'last 2 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions',
+  'last 1 Edge versions',
+];
 
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
