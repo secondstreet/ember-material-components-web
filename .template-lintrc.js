cbb47f4139cc09cb810b979ab8fb1a2a08255f15
Gaurav Munjal <gmunjal@uplandsoftware.com>
2021-11-19 15:53:20 -0500

    fix lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,5 +6,9 @@ module.exports = {
     'no-invalid-interactive': false,
     'no-inline-styles': false,
     'self-closing-void-elements': false,
+    'no-curly-component-invocation': false,
+    'no-yield-only': false,
+    'no-action': false,
+    'no-duplicate-landmark-elements': false,
   },
 };
