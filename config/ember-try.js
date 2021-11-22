fc003433b5ea4fb5815ee00d1a3991710e254a44
Gaurav Munjal <gmunjal@uplandsoftware.com>
2021-11-22 12:45:07 -0500

    drop compatibility with ember < 3.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,46 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-            'ember-source': '~2.16.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-            'ember-source': '~2.18.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
