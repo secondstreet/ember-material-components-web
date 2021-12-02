12883131e26c277cdb3f88653112973e41b07c94
Gaurav Munjal <gmunjal@uplandsoftware.com>
2021-12-02 10:02:53 -0500

    address feedback

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const browsers = [
-  'last 2 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions',
-  'last 1 Edge versions',
-];
+let browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+
+const isCI = Boolean(process.env.CI);
+const isProduction = process.env.EMBER_ENV === 'production';
+const isOldScenario = process.env.EMBER_TRY_SCENARIO === 'ember-lts-3.16';
+
+if (isCI || isProduction) {
+  browsers = [
+    'last 3 Chrome versions',
+    'last 3 iOS versions',
+    'last 2 ChromeAndroid versions',
+    'last 3 Firefox versions',
+    'last 2 Safari versions',
+    'last 3 Edge versions',
+  ];
+}
 
 // Ember's browser support policy is changing, and IE11 support will end in
 // v4.0 onwards.
@@ -18,9 +28,9 @@ const browsers = [
 // const isCI = Boolean(process.env.CI);
 // const isProduction = process.env.EMBER_ENV === 'production';
 //
-// if (isCI || isProduction) {
-//   browsers.push('ie 11');
-// }
+if (isProduction || isOldScenario) {
+  browsers.push('ie 11');
+}
 
 module.exports = {
   browsers,
